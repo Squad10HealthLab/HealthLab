e9d27e72e26e32e284db5bbc93b707b8f9fac5fc
Luca Cariolin <lucacariolin@hotmail.com>
2021-05-28 07:58:34 -0300

    Tela de Listagem de Ficha Clinica

--- Front-end/js/funcoes.js
+++ Front-end/js/funcoes.js
@@ -31,7 +31,14 @@ for(let i = 0; i < inputs.length; i++)
   dateInputMask(inputs[i]);
 }
 
-
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
 function somenteNumero(evt) {
          
---
+++ Front-end/jsonData/fichasClinicas.js
@@ -0,0 +1,38 @@
+data = `
+[
+    {
+        "id": 1,
+        "nome" : "Carlos", 
+        "dataNascimento" : "22/10/1987",
+        "estadoCivil": 0,
+        "filhos": 0,
+        "sexo": 0,
+        "altura": 170,
+        "peso": 80,
+        "acidente": false,
+        "afastamento": true,
+        "modalidade": true,
+        "doencas": "Diabetes TIPO i",
+        "ergonomia": "...",
+        "voz": "Ok",
+        "ocupacionais": "Nenhuma"
+
+    },
+    {
+        "id": 2,
+        "nome" : "João Da Silva", 
+        "dataNascimento" : "10/05/1958",
+        "estadoCivil": 1,
+        "filhos": 5,
+        "sexo": 0,
+        "altura": 180,
+        "peso": 90,
+        "acidente": true,
+        "afastamento": false,
+        "modalidade": false,
+        "doencas": "Nenhuma",
+        "ergonomia": "Prejudicada devido a serviço realizado",
+        "voz": "Ok",
+        "ocupacionais": "Dor lombar"
+    }
+]`;
